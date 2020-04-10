01cd9fb7f97e6d1043799d94e87add1f2969f0f9
Eitan1121 <eitanpl@post.bgu.ac.il>
2020-04-10 16:26:13 -0700

    GUEST USE CASE - 2.1 AND 2.2 IMPLEMENTED.

--- src/main/java/ServiceLayer/GuestService.java
+++ src/main/java/ServiceLayer/GuestService.java
@@ -11,12 +11,17 @@ public class GuestService implements IGuestService{
         this.guest = guest;
     }
 
+    /**
+     * Use Case - 2.1
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param password
+     */
     public void register(String firstName, String lastName, String email, String password){
-        for (char c : password.toCharArray()){
-            if (!((c>='a' && c<='Z')||(c>='0' && c<='9'))){
-                System.out.println("## Password can contain only digits and letters. ##");
-                return;
-            }
+        boolean passwordIsOk = Authentication(password);
+        if(passwordIsOk == false){
+            return;
         }
         boolean isExists = guest.checkIfEmailExists(email);
         if (isExists == true){
@@ -26,7 +31,35 @@ public class GuestService implements IGuestService{
         User newUser = new User(email, password, firstName, lastName);
         Fan fan = new Fan(newUser,firstName);
         newUser.getRoles().add(fan);
-        guest.addNewUser(newUser);
+        guest.addNewUser(newUser,true);
         System.out.println("## Registered to system successfully ##");
+    }
+
+    /**
+     * Use Case - 2.2
+     * @param email
+     * @param password
+     */
+    public void logIn(String email, String password){
+        boolean passwordIsOk = Authentication(password);
+        if(passwordIsOk == false){
+            return;
+        }
+        User userToSignIn = guest.signIn(email, password);
+        if (userToSignIn == null){
+            System.out.println("## Wrong email or password ##");
+            return;
+        }
+        guest.addNewUser(userToSignIn,false);
+    }
+
+    private boolean Authentication(String password){
+        for (char c : password.toCharArray()){
+            if (!((c>='a' && c<='Z')||(c>='0' && c<='9'))){
+                System.out.println("## Password can contain only digits and letters. ##");
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/ServiceLayer/IGuestService.java
+++ src/main/java/ServiceLayer/IGuestService.java
@@ -3,4 +3,6 @@ package ServiceLayer;
 public interface IGuestService {
 
     void register(String firstName, String lastName, String email, String password);
+
+    void logIn(String lastName, String password);
 }
